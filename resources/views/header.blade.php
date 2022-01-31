9144c61fe7886e126794b1e562f51d99a53e352e
Wiwat Krutanang <wiwat@mercular.com>
2022-02-01 05:11:23 +0700

    Update header.blade.php

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -1,6 +1,6 @@
 <div class="card mt-4">
     <div class="card-header d-flex justify-content-between align-items-center">
-        <span class="text-bold">{{ Auth::user()->name }}</span>
+        <span class="text-bold text-primary">{{ Auth::user()->name }}</span>
         <a href="{{ url('/auth/logout') }}" class="btn btn-secondary ml-auto">ออกจากระบบ</a>
     </div>
 </div>
